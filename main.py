6dd7179592e3cb3ec576f82e12533db319e65188
marcoguiidi <123477203+marcoguiidi@users.noreply.github.com>
2025-12-02 16:08:20 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,57 @@
+liste = {
+    "animali": ["cane", "gatto", "elefante", "leone", "tigre", "orso"],
+    "persone": ["giacomo", "mario", "luigi", "anna", "maria", "francesca"]
+}
+
+avaiable_options = list(liste.keys()) + ["e"]
+
+print("Benvenuto nel programma di gestione delle liste!")
+
+
+
+
+
+
+choice = input("\n1 - Visualizza le liste \n"
+    "2 - Aggiungi un elemento a una lista \n"
+    "3 - Cerca un elemento in una lista \n"
+    "q - Esci\n"
+    "Scegli un'opzione: ").lower().strip()
+
+while choice != "q":
+    if choice == "1":
+        for key, value in liste.items():
+            print(f"\nLista di {key}:")
+            for item in value:
+                print(f"- {item}")
+    elif choice == "2":
+        list_choice = input(f"A quale lista vuoi aggiungere un elemento? ({', '.join(avaiable_options)} - indietro ): ").strip().lower()
+        while list_choice not in avaiable_options:
+            list_choice = input(f"A quale lista vuoi aggiungere un elemento? ({', '.join(avaiable_options)} - Indietro): ").strip().lower()
+        if list_choice != "e":
+            new_element = input("Inserisci l'elemento da aggiungere: ").strip().lower()
+            if new_element in liste[list_choice]:
+                    print(f"{new_element} è già presente nella lista {list_choice}.")
+            else:
+                liste[list_choice].append(new_element)
+                print(f"{new_element} è stato aggiunto alla lista {list_choice}.")
+    elif choice == "3":
+        search_el = input("Inserisci il nome dell'elemento da cercare: ").strip().lower()
+        found = False
+        for key, value in liste.items():
+            if search_el in value:
+                print(f"{search_el} è presente nella lista {key}.")
+                found = True
+
+        if not found:
+            print(f"{search_el} non è presente in nessuna delle liste.")
+    else:
+        print("Opzione non valida. Riprova.")
+
+    choice = input("\n1 - Visualizza le liste \n"
+    "2 - Aggiungi un elemento a una lista \n"
+    "3 - Cerca un elemento in una lista \n"
+    "q - Esci\n"
+    "Scegli un'opzione: ").lower().strip()
+
+print("Programma terminato.")
